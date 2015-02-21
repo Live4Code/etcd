bec337f7f8be1cf819131294f1a6beeec58b912e
Yue Chen <y90988@Yues-MacBook-Pro.local>
2015-02-21 01:33:16 -0800

    add run.sh

---
+++ run.sh
@@ -0,0 +1,5 @@
+HOST_IP=$(echo $HOST_IP | sed 's|\[||g')
+HOST_IP=$(echo $HOST_IP | sed 's|\]||g')
+HOST_IP=$(echo $HOST_IP | sed -r "s|(')||g")
+
+/bin/etcd  -name $ETCD_NODE_NAME -data-dir=/data -initial-advertise-peer-urls http://$HOST_IP:2380 -listen-peer-urls http://$HOST_IP:2380 -discovery $DISCOVERY_URL
