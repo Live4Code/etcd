a88caa982073a64a3279cef98c00051c3279757d
Yue Chen <dspfac@gmail.com>
2015-02-23 10:07:15 +0000

    changes the 4001 listener

--- run.sh
+++ run.sh
@@ -4,4 +4,10 @@ HOST_IP=$(echo $HOST_IP | sed 's|\[||g')
 HOST_IP=$(echo $HOST_IP | sed 's|\]||g')
 HOST_IP=$(echo $HOST_IP | sed -r "s|(')||g")
 
-/bin/etcd  -name $ETCD_NODE_NAME -data-dir=/data -initial-advertise-peer-urls http://$HOST_IP:2380 -listen-peer-urls http://$HOST_IP:2380 -discovery $DISCOVERY_URL
+export HOST_IP=$HOST_IP
+
+LOCAL_IP=$(ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://')
+
+export LOCAL_IP=$LOCAL_IP
+
+/bin/etcd  -name $ETCD_NODE_NAME -data-dir=/data -initial-advertise-peer-urls http://$HOST_IP:2380 -listen-peer-urls http://$LOCAL_IP:2380   -listen-client-urls http://$LOCAL_IP:4001 -discovery $DISCOVERY_URL -initial-cluster-state new
