ca0fa19afd042497d03f7623406b374c7923f35a
Yue Chen <dspfac@gmail.com>
2015-06-08 00:29:41 -0700

    update etcd command line

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ LOCAL_IP=$(ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr:/
 
 export LOCAL_IP=$LOCAL_IP
 
-/bin/etcd  -name $ETCD_NODE_NAME -data-dir=/data -initial-advertise-peer-urls http://$HOST_IP:2380 -listen-peer-urls http://$LOCAL_IP:2380   -listen-client-urls http://$LOCAL_IP:4001,  http://$LOCAL_IP:2380 -advertise-client-urls http://$LOCAL_IP:4001, http://$LOCAL_IP:2380 -discovery $DISCOVERY_URL -initial-cluster-state new
+/bin/etcd  -name $ETCD_NODE_NAME -data-dir=/data -initial-advertise-peer-urls http://$HOST_IP:2380 -listen-peer-urls http://$LOCAL_IP:2380   -listen-client-urls http://$LOCAL_IP:4001,http://$LOCAL_IP:2379 -advertise-client-urls http://$LOCAL_IP:4001,http://$LOCAL_IP:2379 -discovery $DISCOVERY_URL -initial-cluster-state new
